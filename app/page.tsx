9ef568397839e5bbd4b6e22b03b3a144a68b4d38
Michael Bradley <jamminwithbiglu@gmail.com>
2026-01-20 15:46:10 -0500

    Wire Veridian engine into Next.js page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 export default function Home() {
   return (
-    <iframe
+    <iframe title="Veridian Q Engine"
+
       src="/veridian-engine.html"
-      style={{ width: "100vw", height: "100vh", border: "none" }}
-      title="Veridian Q"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+      }}
     />
   );
 }
