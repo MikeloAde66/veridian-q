4d0cfdd5f5d2d0bebc5d9a3c127691e744ea8008
Michael Bradley <jamminwithbiglu@gmail.com>
2026-01-20 13:50:09 -0500

    Baseline: Next.js shell hosting Veridian Q engine

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <iframe
+      src="/veridian-engine.html"
+      style={{ width: "100vw", height: "100vh", border: "none" }}
+      title="Veridian Q"
+    />
+  );
+}
